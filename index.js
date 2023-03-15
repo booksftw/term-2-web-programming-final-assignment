f4a4188728693b3144dbdc62c75979bd0d50483d
Nicholas Zachary <nicholas.zachary@outlook.com>
2023-03-15 01:08:42 -0700

    saving

--- index.js
+++ index.js
@@ -25,8 +25,181 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/search', async (req, res) => {
+  console.log(req.query, 'req query')
+
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.pageSize) || 10;
+  const skip = (page - 1) * pageSize;
+  let sampleCount = 0;
+  await Sample.countDocuments({})
+    .then(e => sampleCount = e)
+    ;
+  console.log("SKIP", skip)
+
+  if (req.query.page > 1) {
+    console.log("query page greater than 1")
+    const movies = await Sample.find({})
+      // .skip(5 * page)
+      .skip(skip)
+      .limit(pageSize)
+      .then((movies) => {
+        const totalMovies = sampleCount;
+        const hasNextPage = (skip + pageSize) < totalMovies;
+        res.render('searchResults', { movies: movies, page: page, hasNextPage: hasNextPage, pageSize: pageSize })
+        // console.log(movies)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  } else {
+    const movies = await Sample.find({})
+      .skip(5)
+      .limit(20)
+      .then((movies) => {
+        const totalMovies = movies.length;
+        const hasNextPage = skip + pageSize < totalMovies;
+        res.render('searchResults', { movies: movies, page: page, hasNextPage: hasNextPage, pageSize: pageSize })
+        // console.log(movies)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  // Empty search query should return all results
+  if (req.query.query === "" && req.query.filter2 === "all_categories") {
+    console.log("Rendering all category blank search")
+    const movies = await Sample.find({})
+      .skip(5)
+      .limit(20)
+      .then((movies) => {
+        const totalMovies = movies.length;
+        const hasNextPage = skip + pageSize < totalMovies;
+        res.render('searchResults', { movies: movies, page: page, hasNextPage: hasNextPage, pageSize: pageSize })
+        // console.log(movies)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+  }
+
+
+  if (req.body.filter2 === "all_categories") {
+    switch (req.body.filter1) {
+      case "title":
+        console.log("render title search")
+        // Sample.find({ title: `/(.*)Aileen(.*)/ ` })
+        // Sample.find({ title: new RegExp('/(.*)Aileen(.*)/', 'i') })
+        Sample.find({ title: new RegExp(req.body.query, 'i') })
+          // Sample.find({ title: req.body.query })
+          .limit(10)
+          .then((movies) => {
+            res.render('searchResults', { movies: movies })
+            console.log(movies)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+        break;
+      case "year":
+        console.log("year render")
+        Sample.find({ year: req.body.query })
+          .limit(10)
+          .then((movies) => {
+            res.render('searchResults', { movies: movies })
+            console.log(movies)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+        break;
+      case "runtime-greater":
+        console.log("run time greater")
+        Sample.find({ runtime: { $gt: req.body.query } })
+          .limit(10)
+          .then((movies) => {
+            res.render('searchResults', { movies: movies })
+            console.log(movies)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+        break
+      case "runtime-less":
+        console.log("run time less")
+        Sample.find({ runtime: { $lt: req.body.query } })
+          .limit(10)
+          .then((movies) => {
+            res.render('searchResults', { movies: movies })
+            console.log(movies)
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+        break
+      default:
+        console.log("Something wrong happened")
+        break;
+    }
+  } else {
+    // 2 Filters selected
+    console.log("Two Filter selected NZ")
+    if (req.body.filter1 === "title") {
+      console.log("this title block rendered")
+      Sample.find({ $and: [{ rated: req.body.filter2 }, { title: new RegExp(req.body.query, 'i') }] })
+        .limit(10)
+        .then((movies) => {
+          res.render('searchResults', { movies: movies })
+          // console.log(movies)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+    if (req.body.filter1 === "year") {
+      // Sample.find({ $and: [{ title: req.body.query }, { rated: req.body.filter2 }] })
+      Sample.find({ $and: [{ rated: req.body.filter2 }, { year: req.body.query }] })
+        .limit(10)
+        .then((movies) => {
+          res.render('searchResults', { movies: movies })
+          // console.log(movies)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+      // }
+    }
+
+    if (req.body.filter1 === "runtime-greater") {
+      Sample.find({ $and: [{ rated: req.body.filter2 }, { runtime: { $gt: req.body.query } }] })
+        .limit(10)
+        .then((movies) => {
+          res.render('searchResults', { movies: movies })
+          // console.log(movies)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+
+    if (req.body.filter1 === "runtime-less") {
+      Sample.find({ $and: [{ rated: req.body.filter2 }, { runtime: { $lt: req.body.query } }] })
+        .limit(10)
+        .then((movies) => {
+          res.render('searchResults', { movies: movies })
+          // console.log(movies)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  }
+});
+
 app.post('/search', async (req, res) => {
-  console.log(req.params)
+  console.log(req.query, 'req query')
 
   const page = parseInt(req.query.page) || 1;
   const pageSize = parseInt(req.query.pageSize) || 10;
